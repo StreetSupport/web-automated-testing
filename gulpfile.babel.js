a6cf833f014ae30cdb391a33ce2ed39177ab1d5b
Vince Lee <vslee888@gmail.com>
2016-11-29 14:13:53 +0000

    travis environments

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,28 +6,27 @@ import casperJs from 'gulp-casperjs'
 import download from 'gulp-download'
 import runSequence from 'run-sequence'
 
-const outputDir = '_dist/tests'
-const testDir = 'tests/**/*'
+import * as config from './config'
 
 gulp.task('warm-api', () => {
-  download('https://dev-api-streetsupport.azurewebsites.net/v2/service-categories')
+  download(config.apiUri)
 })
 
 gulp.task('build', () => {
-  gulp.src(testDir + '.js')
+  gulp.src(config.testDir + '.js')
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(gulp.dest(outputDir))
+    .pipe(gulp.dest(config.outputDir))
 })
 
 gulp.task('casper', () => {
-  gulp.src(outputDir + '/**/*Test.js')
+  gulp.src(config.outputDir + '/**/*Test.js')
     .pipe(casperJs())
 })
 
 gulp.task('watch', () => {
-  gulp.watch(testDir, ['build', 'casper'])
+  gulp.watch(config.testDir, ['build', 'casper'])
 })
 
 gulp.task('dev', (callback) => {

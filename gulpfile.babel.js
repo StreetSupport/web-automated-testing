3efe53dfc95370f1635c257973b56aa66ac6e49a
Vince Lee <vslee888@gmail.com>
2016-11-29 19:23:51 +0000

    tidy up

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,25 +7,12 @@ import casperJs from 'gulp-casperjs'
 import download from 'gulp-download'
 import runSequence from 'run-sequence'
 import * as config from './config'
-import del from 'del'
-import mkdirp from 'mkdirp'
 
-
-var fs = require('fs');
 gulp.task('warm-api', () => {
   download(config.apiUri)
 })
 
 gulp.task('build', () => {
-  mkdirp(config.outputDir)
-  fs.readdir('tests/', function(err, items) {
-      console.log(items);
-
-      for (var i=0; i<items.length; i++) {
-          console.log(items[i]);
-      }
-  });
-
   return gulp.src('tests/**/*.js')
     .pipe(babel({
       presets: ['es2015']
@@ -34,26 +21,7 @@ gulp.task('build', () => {
 })
 
 gulp.task('casper', () => {
-  try {
-    const path = __dirname + '/_dist/homeTest.js'
-    const stats = fs.lstatSync(path)
-    console.log(path + ' is file ' + stats.isFile())
-  }
-  catch (e) {
-    console.log('error')
-    console.log(e)
-  }
-
-  fs.readdir('_dist/', function(err, items) {
-      console.log(items);
-
-      for (var i=0; i<items.length; i++) {
-          console.log(items[i]);
-      }
-  });
-
-
-  return gulp.src('_dist/homeTest.js')
+  return gulp.src('_dist/**/*.js')
     .pipe(foreach((stream, file) => {
       console.log('test:' + file.relative)
       return stream

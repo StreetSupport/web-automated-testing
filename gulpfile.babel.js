3bba6eb2c19a1efae12038dd38553805b52435cc
Vince Lee <vslee888@gmail.com>
2016-11-29 18:52:25 +0000

    parp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,12 +7,19 @@ import casperJs from 'gulp-casperjs'
 import download from 'gulp-download'
 import runSequence from 'run-sequence'
 import * as config from './config'
+import del from 'del'
+
+
 var fs = require('fs');
 gulp.task('warm-api', () => {
   download(config.apiUri)
 })
 
 gulp.task('build', () => {
+  del([
+    config.outputDir
+  ])
+
   gulp.src(config.testDir + '.js')
     .pipe(babel({
       presets: ['es2015']

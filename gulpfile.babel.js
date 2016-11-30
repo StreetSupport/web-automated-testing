e74424ea995b595ef95252c4125cdb5420674cf9
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2016-11-30 10:28:19 +0000

    tidy up

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,16 +2,13 @@
 
 import gulp from 'gulp'
 import babel from 'gulp-babel'
-import foreach from 'gulp-foreach'
 import casperJs from 'gulp-casperjs'
 import download from 'gulp-download'
 import runSequence from 'run-sequence'
 import * as config from './config'
 import del from 'del'
-import mkdirp from 'mkdirp'
 
 
-var fs = require('fs');
 gulp.task('warm-api', () => {
   download(config.apiUri)
 })
@@ -29,11 +26,10 @@ gulp.task('build', () => {
 })
 
 gulp.task('casper', () => {
-  return gulp.src(config.outputDir + '/**/*Test.js')
-    .pipe(foreach((stream, file) => {
-      console.log('test: ' + file.relative)
-      return stream
-        .pipe(casperJs())
+  return gulp
+    .src(config.outputDir + '/**/*Test.js')
+    .pipe(casperJs({
+      binPath: './node_modules/casperjs/bin/casperjs'
     }))
 })
 

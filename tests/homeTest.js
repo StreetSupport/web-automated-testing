6db5ec1d73c8f253172214d5f677efc51bd9ed8f
Vince Lee <vslee888@gmail.com>
2016-12-06 08:38:54 +0000

    wait for manchester to kick in

--- tests/homeTest.js
+++ tests/homeTest.js
@@ -6,7 +6,9 @@ casper.test.begin('Home', 1, function (test) {
   new Browser(phantom).setLocation('manchester')
   casper.start(pages.home.url, function () {
     casper.capture('./captures/home/initial-load.png')
-    test.assertSelectorHasText('.block__header', 'Street Support in Manchester')
+    casper.waitUntilVisible('.nav__item--manchester', () => {
+      test.assertSelectorHasText('.block__header', 'Street Support in Manchester')
+    })
   })
 
   casper.run(() => {
